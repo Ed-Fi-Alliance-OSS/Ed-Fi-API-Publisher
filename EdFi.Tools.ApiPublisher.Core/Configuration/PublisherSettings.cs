94ad413d6eef6c68cea9301040f05aebde3c775b
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2021-05-27 21:39:20 -0500

    Added logic to the MaxDegreeOfParallelismForPostResourceItem to prevent overly large values from being set, but using the .NET Core reported maximum thread count available, capped at 200.

--- EdFi.Tools.ApiPublisher.Core/Configuration/PublisherSettings.cs
+++ EdFi.Tools.ApiPublisher.Core/Configuration/PublisherSettings.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Threading;
+using log4net;
+
 namespace EdFi.Tools.ApiPublisher.Core.Configuration
 {
     public class PublisherSettings
@@ -12,16 +16,47 @@ namespace EdFi.Tools.ApiPublisher.Core.Configuration
         public string Path { get; set; }
         public string[] UpdatePrerequisitePaths { get; set; }
     }
-    
+
     public class Options
     {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(Options));
+        
         public int BearerTokenRefreshMinutes { get; set; } = 12;
         
         public int RetryStartingDelayMilliseconds { get; set; } = 250;
         
         public int MaxRetryAttempts { get; set; } = 5;
 
-        public int MaxDegreeOfParallelismForPostResourceItem { get; set; } = 200;
+        private int _maxDegreeOfParallelismForPostResourceItem;
+        
+        public int MaxDegreeOfParallelismForPostResourceItem
+        {
+            get => _maxDegreeOfParallelismForPostResourceItem;
+            set
+            {
+                if (value <= 0)
+                {
+                    _logger.Warn($"Attempted max parallelism of '{value}' for posting resources is invalid. Setting has been adjusted to '1'.");
+                    _maxDegreeOfParallelismForPostResourceItem = 1;
+
+                    return;
+                }
+                
+                // Limit setting to the number of threads available
+                ThreadPool.GetMaxThreads(out int workerThreadCount, out int completionPortThreadCount);
+
+                // Cap the maximum parallelization at a reasonable level of 200
+                // (GetMaxThreads could return a number as high as 32,767 depending on the environment)
+                int practicalMaxParallelization = Math.Min(200, workerThreadCount);
+
+                _maxDegreeOfParallelismForPostResourceItem = Math.Min(value, practicalMaxParallelization);
+                
+                if (value > _maxDegreeOfParallelismForPostResourceItem)
+                {
+                    _logger.Warn($"Attempted max parallelism of '{value}' for posting resources is too large. Setting has been adjusted to '{_maxDegreeOfParallelismForPostResourceItem}'.");
+                }
+            }
+        }
 
         public int MaxDegreeOfParallelismForStreamResourcePages { get; set; } = 10;
 
