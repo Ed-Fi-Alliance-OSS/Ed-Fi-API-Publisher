edb79ea6e43f763da50c5a1ddb8d021050d62d44
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-12-02 00:10:45 -0600

    Minor cleanup of unused namespaces.

--- EdFi.Tools.ApiPublisher.Core/Processing/Blocks/StreamResourcePagesBlockFactory.cs
+++ EdFi.Tools.ApiPublisher.Core/Processing/Blocks/StreamResourcePagesBlockFactory.cs
@@ -1,20 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using EdFi.Tools.ApiPublisher.Core.Configuration;
-using EdFi.Tools.ApiPublisher.Core.Extensions;
-using EdFi.Tools.ApiPublisher.Core.Helpers;
 using EdFi.Tools.ApiPublisher.Core.Processing.Handlers;
 using EdFi.Tools.ApiPublisher.Core.Processing.Messages;
 using log4net;
-using Newtonsoft.Json.Linq;
-using Polly;
-using Polly.Contrib.WaitAndRetry;
 
 namespace EdFi.Tools.ApiPublisher.Core.Processing.Blocks
 {
